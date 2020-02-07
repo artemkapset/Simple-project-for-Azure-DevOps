8a83a12b00a52d732cb7b27334342e1fdc5baf49
Artsiom Kapset <a.kapset@godeltech.com>
2020-02-07 12:03:41 +0300

    Add project files.

---
+++ src/TechFabricSln/Bought.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechFabricSln
+{
+    public class Bought
+    {
+        public User MadeBy { get; set; }
+
+        public bool IsBoughtBy(User user)
+        {
+            if (user.Customer)
+            {
+                return true;
+            }
+            if (MadeBy == user)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ test/TechFabricSln.Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using TechFabricSln;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void IsBoughtBy_UserIsCustomer_ReturnTrue()
+        {
+            var bought = new Bought();
+            var result = bought.IsBoughtBy(new User { Customer = true });
+            Assert.IsTrue(result);
+        }
+    }
+}
